8d9480185052f4b4de203636c2d7eae9eacc5896
Joao Carlos Alves <suporte_carlos@live.com>
2022-03-30 18:10:59 -0300

    New Exercicie to JAVA compose

---
+++ composicao/src/entities/workerlevel/ScreenEfe.java
@@ -0,0 +1,19 @@
+package entities.workerlevel;
+
+public class ScreenEfe {
+
+	public static void screen() {
+		System.out.println("|||||||||||||||||||||||||||||");
+		System.out.println("|||||||||||||||||||||||||||||");
+		System.out.println("|||||||||||||||||||||||||||||");
+		System.out.println("||||||SISTEMA CONTABIL|||||||");
+		System.out.println("|||||||||||||||||||||||||||||");
+		System.out.println("||||||||||||CONTROLE|||||||||");
+		System.out.println("|||||||||||||||||||||||||||||");
+		System.out.println("|||||||||||||||||||||||||||||");
+		System.out.println("|||||DE HORAS||||||||||||||||");
+		System.out.println("|||||||||||||||||||||||||||||");
+		System.out.println("|||||||||||||||||||||||||||||");
+		System.out.println("|||||||||||||||||||||||||||||");
+	}
+}
